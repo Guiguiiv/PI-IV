b56edd328e2032752c25315bdcd85789c1071e1c
Guilherme Domiciano de Viveiros <guilhermedmc00@gmail.com>
2025-04-10 14:42:37 -0300

    Adiciona relacionamento com Cliente na entidade Endereco e ajusta atributos

--- src/main/java/com/PI_IV/model/Endereco.java
+++ src/main/java/com/PI_IV/model/Endereco.java
@@ -1,4 +1,50 @@
 package com.PI_IV.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "enderecos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco")
+    private int id;
+
+    @Column(name = "logradouro", length = 100, nullable = false)
+    private String logradouro;
+
+    @Column(name = "cep", length = 9, nullable = false)
+    private String cep;
+
+    @Column(name = "bairro", length = 100, nullable = false)
+    private String bairro;
+
+    @Column(name = "uf", length = 2, nullable = false)
+    private String uf;
+
+    @Column(name = "cidade", length = 100, nullable = false)
+    private String cidade;
+
+    @Column(name = "numero", length = 4, nullable = false)
+    private String numero;
+
+    @Column(name = "complemento", length = 20)
+    private String complemento;
+
+    @Column(name = "tipoendereco")
+    private boolean tipoEndereco;
+
+    @Column(name = "principal")
+    private boolean principal;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
 }

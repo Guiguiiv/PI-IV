ff662cd6a95ec3611bf91ce2bc8e7ccff0092c12
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-04-25 15:04:25 -0300

    Corrigir bug cadastro endereço e melhorias no css

--- src/main/java/com/PI_IV/controller/EnderecoController.java
+++ src/main/java/com/PI_IV/controller/EnderecoController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/enderecos")
+@RequestMapping("/endereco")
 public class EnderecoController {
 
     @Autowired
